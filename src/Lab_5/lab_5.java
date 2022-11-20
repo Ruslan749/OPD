e8719f1946898f55d994d6f3844d0107d1c154a8
Ruslan <aleks99154@yandex.ru>
2022-11-20 21:10:30 +0500

    lab_5

--- src/Lab_5/lab_5.java
+++ src/Lab_5/lab_5.java
@@ -150,7 +150,6 @@ class Methods{
         errorSumDiviation = Math.round((float) sumMeanDiviation / sumLabor * 100);
         return errorSumDiviation;
     }
-
     // Расчёт общей трудоёмкости
     int _sumDiviation(){
         _sumDiviation = 4 * sumDiviation;
