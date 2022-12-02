499d46210ccc4f85c1cc626073b30027d8ea203e
Ruslan <aleks99154@yandex.ru>
2022-12-03 00:42:07 +0500

    labarotornaaya_5 v1.1

--- src/Lab_5/lab_5.java
+++ src/Lab_5/lab_5.java
@@ -1,68 +1,16 @@
 package Lab_5;
 import java.util.Scanner;
 public class lab_5 {
-    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         Methods methods = new Methods();
 // Для тестового расчета будем считать, что согласно этой статистике, разработка и отладка требовала у программиста:
 
-//        int [] screen = new int[]{2,20,4}; // для одного экрана
-//        int [] event  = new int[]{4,32,8}; // для одного обработчика событий
-//        int [] businessObject = new int[]{2,8,3}; // для одного бизнес объекта
-//        int [] newBusinessMethod = new int[]{2,26,6}; // для одого нового бизнес метода
-
-        int [] screen  = new int[3];
-        int [] event  = new int[3];
-        int [] businessObject = new int[3];
-        int [] newBusinessMethod = new int[3];
-
-        for (int j = 0; j <= 1; j++){
-            System.out.println("трудозатраты для одного экрана");
-            for (int i = 0; i< screen.length; i++){
-                switch (i) {
-                    case 0 -> System.out.print("минимально возможные трудозатраты: ");
-                    case 1 -> System.out.print("пессимистическая оценка трудозатрат: ");
-                    case 2 -> System.out.print("наиболее вероятная оценка трудозатрат: ");
-                }
-                screen[i] = in.nextInt();
-            }
-        }
-
-        for (int j = 0; j<=1; j++){
-            for (int i = 0; i< event.length; i++){
-                System.out.println("трудозатраты для одного обработчика событий");
-                switch (i) {
-                    case 0 -> System.out.print("минимально возможные трудозатраты: ");
-                    case 1 -> System.out.print("пессимистическая оценка трудозатрат: ");
-                    case 2 -> System.out.print("наиболее вероятная оценка трудозатрат: ");
-                }
-                event[i] = in.nextInt();
-            }
-        }
-
-        for (int j = 0; j<=1; j++){
-            for (int i = 0; i< businessObject.length; i++){
-                System.out.println("трудозатраты для одного бизнес объекта");
-                switch (i) {
-                    case 0 -> System.out.print("минимально возможные трудозатраты: ");
-                    case 1 -> System.out.print("пессимистическая оценка трудозатрат: ");
-                    case 2 -> System.out.print("наиболее вероятная оценка трудозатрат: ");
-                }
-                businessObject[i] = in.nextInt();
-            }
-        }
-
-        for (int j = 0; j<=1; j++){
-            for (int i = 0; i< newBusinessMethod.length; i++){
-                System.out.println("трудозатраты для одого нового бизнес метода");
-                switch (i) {
-                    case 0 -> System.out.print("минимально возможные трудозатраты: ");
-                    case 1 -> System.out.print("пессимистическая оценка трудозатрат: ");
-                    case 2 -> System.out.print("наиболее вероятная оценка трудозатрат: ");
-                }
-                newBusinessMethod[i] = in.nextInt();
-            }
-        }
+        int [] screen = new int[]{2,20,4}; // для одного экрана
+        int [] event  = new int[]{4,32,8}; // для одного обработчика событий
+        int [] businessObject = new int[]{2,8,3}; // для одного бизнес объекта
+        int [] newBusinessMethod = new int[]{2,26,6}; // для одого нового бизнес метода
+        methods.valueScaner();
 
         System.out.print("Оценку средней трудоемкости кодирования по каждому элементарному пакету: \n");
         System.out.printf(" - трудоемкомть кодирования для экранов: %s \n",methods.meanLaborCod(screen)); // трудоемкомть кодирования для экранов
@@ -77,7 +25,7 @@ public class lab_5 {
         System.out.printf(" - для новых бизнес методов: %s \n",methods.meanDiviation(newBusinessMethod));
 
         System.out.print("Суммарная трудоемкость проекта: ");
-        System.out.println(methods.sumLabor(methods.meanLaborCod(screen),methods.meanLaborCod(event),methods.meanLaborCod(businessObject),methods.meanLaborCod(newBusinessMethod)));
+        System.out.println(methods.sumLabor( methods.meanLaborCod(screen),  methods.meanLaborCod(event),  methods.meanLaborCod(businessObject), methods.meanLaborCod(newBusinessMethod)));
 
         System.out.print("Среднеквадратичное отклонение для оценки суммарной трудоемкости кодирования: ");
         System.out.println(methods.sumMeanDiviation(methods.meanDiviation(screen),methods.meanDiviation(event),methods.meanDiviation(businessObject),methods.meanDiviation(newBusinessMethod)));
@@ -103,71 +51,106 @@ public class lab_5 {
 }
 
 class Methods{
-    private int meanLaborCod;
-    private  int meanDiviation;
-    private int sumLabor;
-    private int sumMeanDiviation;
-    private  int sumDiviation;
-    private int errorSumDiviation;
-    private int _sumDiviation;
-    private int recPeople;
-    private float law;
+    static Scanner in = new Scanner(System.in);
+//    int [] data = new int[]{20,60,16,40};
+    int [] data = new int[4];
+    public void valueScaner (){
+        for (int j = 0; j < 1; j++){
+            for (int i = 0; i< data.length; i++){
+                System.out.println("новое разрабатываемое приложение содержит");
+                switch (i) {
+                    case 0 -> System.out.print("пользовательские экраны: ");
+                    case 1 -> System.out.print("обработчики событий: ");
+                    case 2 -> System.out.print("новые бизнес-объекты: ");
+                    case 3 -> System.out.print("новые бизнесметоды: ");
+                }
+                data[i] = in.nextInt();
+            }
+        }
+    }
+
+    private double meanLaborCod;
+    private double meanDiviation;
+    private double sumLabor;
+    private double sumMeanDiviation;
+    private double sumDiviation;
+    private long errorSumDiviation;
+    private double _sumDiviation;
+    private double recPeople;
+    private double law;
     private int meanPeople;
 
+
     // метод оценки средней трудоемкости кодирования по каждому элементарному пакету
-    int  meanLaborCod (int[] num){
+    double meanLaborCod (int[] num){
         for (int i = 0 ; i < num.length; i++){
-            meanLaborCod = (num[0] + 4*num[1] + num[2])/6;
+            double value  = (double)(num[0] + 4*num[2] + num[1])/6;
+            double scale = Math.pow(10, 2);
+            meanLaborCod =  (Math.ceil(value * scale) / scale);
         }
         return meanLaborCod;
     }
     // метод среднеквадратного отклонения
-    int meanDiviation (int [] num){
+    double meanDiviation (int [] num){
         for (int i = 0 ; i < num.length; i++){
-            meanDiviation = (num[1] - num[0])/6;
+            double value  = (double)(num[1] - num[0])/6;
+            double scale = Math.pow(10, 2);
+            meanDiviation = (Math.ceil(value * scale) / scale);
         }
         return meanDiviation;
     }
     // метод суммарной трудоемкости проекта:
-    int sumLabor(int a, int b, int c, int d){
-        sumLabor = a + b + c + d;
+    double sumLabor(double a, double b, double c, double d){
+        for (int i = 0; i< data.length; i++){
+            sumLabor = data[0]*a+data[1]*b+data[2]*c+data[3]*d;
+        }
         return sumLabor;
     }
     //Среднеквадратичное отклонение для оценки суммарной трудоемкости кодирования
-    int sumMeanDiviation (int a, int b, int c, int d){
-        int res = a+b+c+d;
-        sumMeanDiviation = (int) Math.sqrt(Math.pow(res,2));
+    double sumMeanDiviation (double a, double b, double c, double d){
+        for (int i = 0; i< data.length; i++){
+            double res = data[0]*Math.pow(a,2)+data[1]*Math.pow(b,2)+data[2]*Math.pow(c,2)+data[3]*Math.pow(d,2);
+            double value  = Math.sqrt(res);
+            double scale = Math.pow(10, 2);
+            sumMeanDiviation =  (Math.ceil(value * scale) / scale);
+        }
         return sumMeanDiviation;
     }
     // Cуммарной трудоемкости кодирования проекта,
-    int sumDiviation (){
-        sumDiviation = sumLabor + 2 * sumMeanDiviation;
+    double sumDiviation (){
+        double value  = sumLabor + 2 * sumMeanDiviation;
+        double scale = Math.pow(10, 2);
+        sumDiviation = (Math.ceil(value * scale) / scale);
         return sumDiviation;
 
     }
     // Относительная погрешность в оценке суммарной трудоемкости
-    int errorSumDiviation (){
-        errorSumDiviation = Math.round((float) sumMeanDiviation / sumLabor * 100);
+    double errorSumDiviation (){
+        errorSumDiviation = Math.round(sumMeanDiviation / sumLabor * 100);
         return errorSumDiviation;
     }
     // Расчёт общей трудоёмкости
-    int _sumDiviation(){
+    double _sumDiviation(){
         _sumDiviation = 4 * sumDiviation;
         return _sumDiviation;
     }
     // Пересчёт человеко-часов в человеко-месяцы
-    int recPeople (){
-        recPeople = _sumDiviation / 132;
+    double recPeople (){
+        double value  = _sumDiviation / 132;
+        double scale = Math.pow(10, 2);
+        recPeople = (Math.ceil(value * scale) / scale);
         return recPeople;
     }
     // Закон Барии Боэма (Barry Boehm)
-    float law (){
-        law = (float) ((Math.pow(recPeople,0.33))*2.5);
+    double law (){
+        double value  = ((Math.pow(recPeople,0.33))*2.5);
+        double scale = Math.pow(10, 2);
+        law = (Math.ceil(value * scale) / scale);
         return law;
     }
     // Средняя численность команды:
     int meanPeople (){
-        meanPeople = Math.round(recPeople / law);
+        meanPeople = (int) Math.round(recPeople / law);
         return meanPeople;
     }
 }
